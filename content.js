ee2f95f511a00e52faa6ff107e3d62db4e323ff3
sammieatkins <104380965+sammieatkins@users.noreply.github.com>
2024-10-19 11:43:45 -0600

    fixing url matching and some css

--- content.js
+++ content.js
@@ -2,18 +2,51 @@ if (typeof data === 'undefined') {
     var data = "placeholder text for testing";
 }
 
+async function rek(base64) {
+    const response = await fetch('https://ckgqos29n6.execute-api.us-east-1.amazonaws.com/dev', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            base64_image: base64
+        })
+    });
+    const data = await response.json();
+    console.log(data.ocrText);
+    return data.ocrText;
+}
+
 function addContent(image, data) {
     const alt = image.getAttribute('alt') || data;
-    const p = document.createElement('p');
-    p.setAttribute('class', 'eyesite');
-    image.insertAdjacentElement('afterend', p);
-    p.innerHTML = `${alt}`;
 
-    addStyles();
+    const div = document.createElement('div')
+    div.setAttribute('class', 'eyesite')
+
+    const img = document.createElement('img')
+    img.setAttribute('src', 'EyeSite_yellow_transparent.png')
+    img.setAttribute('alt', '')
+
+    const p = document.createElement('p')
+    
+    div.appendChild(img)
+    div.appendChild(p)
+
+    image.insertAdjacentElement('afterend', div)
+    p.innerHTML = `${alt}`
+
+    addStyles()
 }
 
 function handleImage(imageUrl) {
-    const img = document.querySelector(`img[src="${imageUrl}"]`);
+    var img = document.querySelector(`img[src="${imageUrl}"]`);
+    console.log(img)
+    if (imageUrl.startsWith('http')) {
+        const urlObj = new URL(imageUrl);
+        imageUrl = urlObj.pathname;
+        img = document.querySelector(`img[src="${imageUrl}"]`)
+        console.log(imageUrl)
+    }
     if (img) {
         addContent(img, data);
     } else {
